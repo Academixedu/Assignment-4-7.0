7b3898321b1877ec9cad966b098a435258556624
2110Abhishek <agchoudhari2110@gmail.com>
2024-07-18 23:42:12 +0530

    Update Person.java

--- Person.java
+++ Person.java
@@ -3,4 +3,44 @@ public class Person{
   //Create Getters and Setters
   // Implement Constructor argumented
   // Include private
-}
+   public int Eid;
+   private String Ename;
+   public int Esal;
+   private String Eadd;
+             
+   //CONSTRUCTOR
+   public Person (int Eid , String Ename , int Esal , String Eadd){
+   this.Eid=21;         
+   this.Ename="Abhishek";
+   this.Esal=12345;
+   this.Eadd="pardi";}
+
+    //Getter Setter
+
+    public int getEid(){
+      return Eid;
+    }
+    public void setEid(int Eid){
+      this.Eid=Eid;
+    }
+    public String getEname(){
+      return Ename;
+    }
+    public void setEname(String Ename){
+      this.Ename=Ename;
+    }
+    public int getEsal(){
+      return Esal;
+    }
+    public void  setEsal(int Esal){
+      this.Esal=Esal;
+    }
+    public String getEadd(){
+      return Eadd;
+    }
+    public void setEadd(String Eadd){
+      this.Eadd=Eadd;
+    }
+  
+
+   }
