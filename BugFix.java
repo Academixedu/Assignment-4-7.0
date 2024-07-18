f3acc032a6a2e4b901352e1f59a7e6404ca5e48d
syamkuamrnallamilli0 <syamkumarnallamilli0@gmail.com>
2024-07-18 16:16:52 +0530

    commit

--- BugFix.java
+++ BugFix.java
@@ -7,7 +7,7 @@ public class BugFix{
 }
 
 public void setBid(int bid) {
-    Bid = bid;
+    this.Bid = bid;
 }
 
 public String getBname() {
@@ -15,7 +15,7 @@ public String getBname() {
 }
 
 public void setBname(String bname) {
-    Bname = bname;
+    this.Bname = bname;
 }
 
 public BugFix(int bid, String bname) {
@@ -24,10 +24,10 @@ public BugFix(int bid, String bname) {
 }
 
 public static void main(String[]args){
- BugFix b=new BugFix();
- String change=" "; 
+ BugFix b=new BugFix(2,"kumar");
+ String change=" we have to add this keywords to setbid and setname methods,assign values in the object "; 
   b.setBid(1);
-  b.setBname("");
+  b.setBname("syam");
   System.out.println(b.getBid());
   System.out.println(b.getBname());
   }}
--- Test.java
+++ Test.java
@@ -1,10 +1,39 @@
 public class Test{
+  public String course;
+  public int fee;
+  public Test(String course,int fee){
+    this.course=course;
+    this.fee=fee;
+  }
+  public String getcourse(){
+    return course;
+  }
+  public int getfee(){
+    return fee;
+  }
+  public void setfee(int feee){
+    this.fee=feee;
+  }
 public static void main(String[]args){
+Test t1=new Test("java fullstack", 30000);
+Test t2=new Test("dotnet", 40000);
+System.out.println("course:"+t1.getcourse());
+System.out.println("fee:"+t2.getfee());
+t1.setfee(60000);
+System.out.println("update fee:"+t1.getfee());
+
+
+
+
+
 // Create Object for Person
   // Initialize values with Constructors
   // print the values
   // Create another Object for Person
   // Assign Values with Getters & Setters
   // print the values
+
 }
+
+
 }
