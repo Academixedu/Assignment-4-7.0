75fce4803e6c862c520527c5c844279fff42e145
jangamswarupa-595 <swarupajangam9@gmail.com>
2024-07-18 15:48:37 +0530

    assign4

--- WhileLoops.java
+++ WhileLoops.java
@@ -3,7 +3,7 @@ public static void main(String[]args){
   int a=30;
 int i=0;
   while(i<a){
-    System.out.println("Hello World "+a);
+    System.out.println("Hello World "+i);
     i++;
 }
   String change=">,--";
