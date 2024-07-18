441d0f9f713889a0855616ff4af19f9d595dbfb9
2110Abhishek <agchoudhari2110@gmail.com>
2024-07-18 23:41:52 +0530

    Update IfElse.java

--- IfElse.java
+++ IfElse.java
@@ -6,8 +6,27 @@
 // Like Wise the person who gets less than 36 is considered as Grade F
 // Variables should use
 public class IfElse{
-  public int score=60;
+  public int score=80;
   public static void main(String[]args){
 IfElse f=new IfElse();
+if (f.score>90 && f.score<=100){
+  System.out.println("A+ grade");
+}
+else if (f.score>80 && f.score<=90){
+  System.out.println("A grade");
+}
+else if (f.score>70 && f.score<=80){
+  System.out.println("B+ grade");
+}
+else if (f.score>60 && f.score<=50){
+  System.out.println("B grade");
+}
+else if (f.score>00 && f.score<=50){
+  System.out.println("F grade");
+}
+else{
+System.out.println("Invalid Score");
+}
    // Complete the Reamaining Logic using score variable for a value
+}
 }
